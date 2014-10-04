f704088603780e8d95973a3bf34158f961c9bc32
BruceZCQ <brucezcq@gmail.com>
2014-10-04 16:18:04 +0800

    Initial Commit

---
+++ OOHttpKit/OOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OOAppDelegate.h
+//  OOHttpKit
+//
+//  Created by BruceZCQ on 10/4/14.
+//  Copyright (c) 2014 OpeningO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OOHttpKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OOHttpKit
+//
+//  Created by BruceZCQ on 10/4/14.
+//  Copyright (c) 2014 OpeningO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OOAppDelegate class]));
+    }
+}
